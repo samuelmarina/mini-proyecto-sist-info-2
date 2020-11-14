e85008d2ccfb370c5bfb1202b1c9729388fa9da5
samuelmarina <samuellmarina@gmail.com>
2020-11-14 18:26:31 -0400

    Initialize character screen

--- src/app/screens/character/character.component.ts
+++ src/app/screens/character/character.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Character } from 'src/app/schemas/character';
+import { CharacterService } from 'src/app/services/character/character.service';
 
 @Component({
   selector: 'app-character',
@@ -6,8 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./character.component.css']
 })
 export class CharacterComponent implements OnInit {
+  character: Character;
+  id;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private charService: CharacterService) { 
+    this.id = this.route.snapshot.paramMap.get('id');
+
+    this.charService.getCharacterById(this.id).subscribe(res => {
+      this.character = res as Character;
+      console.log(this.character);
+
+    })
+  }
 
   ngOnInit(): void {
   }
